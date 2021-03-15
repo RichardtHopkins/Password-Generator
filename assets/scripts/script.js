9e28380ec9780d56c613f9fd03fd3c12f6958ab3
richard hopkins <richard.t.hopkins@outlook.com>
2021-03-15 13:32:31 +1100

    Made changes to the file structure and how the index html links. Got the generate password button to display manually entered strings. added arrays for letters number and special characters.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,41 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// "arrays" of characters that can be used.
+var alphabetArray = "abcdefghijklmnopqrstuvwxyz";
+var numbersArray = "1234567890";
+var specialCharArray = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+
+console.log(alphabetArray[3].toUpperCase());
+console.log(numbersArray[5]);
+console.log(specialCharArray[0]);
+
+var letters = false;
+var numbers = false;
+var upperCase = false;
+var lowerCase = false;
+var specialCharacters = false;
+var length = 8;
+
+//generatePassword function needs to be created.
+function generatePassword(){
+
+  var password = "test";
+
+  return password;
+}
+
+
+//password criteria. Letters, numbers, Uppercase, lowercase, special character, lenght of password.
+//password must be between 8 and 128 character long.
+
+
+//select array randomly
+//select charcter from array. if it is from the letters array and only the upper case option is selected make sure it is uppercase
+//if only lowercase is selected select only lowercase if both are selected randomly decide if the character is upper or lower.
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
