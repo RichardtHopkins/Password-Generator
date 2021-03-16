cc174a1c734c6137e49c67885ff249a1010ccd25
Richard Hopkins <richard.t.hopkins@outlook.com>
2021-03-17 01:38:44 +1100

    Added Prompts and confirm statements and added in arrays.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,39 +2,67 @@
 var generateBtn = document.querySelector("#generate");
 
 // "arrays" of characters that can be used.
-var alphabetArray = "abcdefghijklmnopqrstuvwxyz";
-var numbersArray = "1234567890";
-var specialCharArray = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-
-
-console.log(alphabetArray[3].toUpperCase());
-console.log(numbersArray[5]);
-console.log(specialCharArray[0]);
-
-var letters = false;
-var numbers = false;
-var upperCase = false;
-var lowerCase = false;
-var specialCharacters = false;
-var length = 8;
+var alphabetLowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+var alphabetUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var numbersArray = "1234567890".split("");
+var specialCharArray = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
+var passwordArraySelction = [];
+var myPassword = [];
 
 //generatePassword function needs to be created.
 function generatePassword(){
-
   var password = "test";
+  var includeUpperCase = false;
+  var includeLowerCase = false;
+
+  //prompts for user to enter a password length between 8 and 128 and repeats if the entered value does not match.
+  var passwordLengthPrompt = window.prompt("Pick a password length between 8 and 128 characters."); 
+  //confirms that the entered amount was valid.
+  if(parseInt(passwordLengthPrompt) >= 8 && parseInt(passwordLengthPrompt) <= 128){
+    var lengthOfPassword = passwordLengthPrompt;
+  }else{
+    window.alert("The entered amount was not valid please press ok to restart.");
+    generatePassword();
+  }
+
+  //checks if the user would like to use letters. and if they do weather they want uppercase and lowercase.
+  var includeLetters = window.confirm("Do you want letters in your password?\n" + "Click okay for yes and cancel for no.");
+  if(includeLetters){
+    includeUpperCase = window.confirm("Would you like uppercase letters?\n" + "Click okay for yes and cancel for no.");
+    if(includeUpperCase)passwordArraySelction.push("uppercase");
+    includeLowerCase = window.confirm("Would you like lowercase letters?\n" + "Click okay for yes and cancel for no.");
+    if(includeLowerCase)passwordArraySelction.push("lowercase");
+  }
+
+  //checks weather the user would like to user numbers in the password.
+  var includeNumbers = window.confirm("Do you want numbers in your password?\n" + "Click okay for yes and cancel for no.");
+  if(includeNumbers)passwordArraySelction.push("numbers");
+
+  var includeSpecialCharacters = window.confirm("Do you want Special Characters. in your password?\n" + "Click okay for yes and cancel for no.");
+  if(includeSpecialCharacters)passwordArraySelction.push("specialcharacters");
+  
+  //if includeuppercase is true add the uppercase alphabet to the my password array.
+  if(passwordArraySelction.indexOf("uppercase") !== -1){
+    myPassword.push(alphabetUpperCase);
+    console.log(myPassword);
+  }
+
+  if(passwordArraySelction.indexOf("lowercase") !== -1){
+    myPassword.push(alphabetLowerCase);
+  }
+
+  if(passwordArraySelction.indexOf("numbers") !== -1){
+    myPassword.push(numbersArray);
+  }
+
+  if(passwordArraySelction.indexOf("specialcharacters") !== -1){
+    myPassword.push(specialCharArray);  
+  }
+
+  console.log(myPassword[25]);
 
   return password;
 }
-
-
-//password criteria. Letters, numbers, Uppercase, lowercase, special character, lenght of password.
-//password must be between 8 and 128 character long.
-
-
-//select array randomly
-//select charcter from array. if it is from the letters array and only the upper case option is selected make sure it is uppercase
-//if only lowercase is selected select only lowercase if both are selected randomly decide if the character is upper or lower.
-
 
 // Write password to the #password input
 function writePassword() {
