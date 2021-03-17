926bfefe2a90b70c1fdef0148f7d827820d764d0
richard hopkins <richard.t.hopkins@outlook.com>
2021-03-17 22:49:42 +1100

    Completed functionality of code and can now generate a password according to criteria

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,15 +5,38 @@ var generateBtn = document.querySelector("#generate");
 var alphabetLowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 var alphabetUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var numbersArray = "1234567890".split("");
-var specialCharArray = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
-var passwordArraySelction = [];
-var myPassword = [];
+var specialCharArray = " !#$%&'()*+-./:;<=>?@[\]^_`{|}~'" .split("");
+
+
+//shuffles generated password entered by the 
+function shuffleArray(passwordArray) {
+  var i, j, x;
+  for (i = passwordArray.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = passwordArray[i];
+      passwordArray[i] = passwordArray[j];
+      passwordArray[j] = x;
+  }
+  return passwordArray;
+}
+
+
+function randomSelection(passwordArray, length){
+  var newPassword = [];
+  var i, selectedIndex;
+  for (i = 1 ; i <= length; i++) {
+    selectedIndex = Math.floor(Math.random() * passwordArray.length);
+    newPassword.push(passwordArray[selectedIndex]);
+    console.log(newPassword);
+  }
+  return newPassword;
+}
 
 //generatePassword function needs to be created.
 function generatePassword(){
-  var password = "test";
-  var includeUpperCase = false;
-  var includeLowerCase = false;
+  
+  //creates blank password array.
+  var myPassword = [];
 
   //prompts for user to enter a password length between 8 and 128 and repeats if the entered value does not match.
   var passwordLengthPrompt = window.prompt("Pick a password length between 8 and 128 characters."); 
@@ -25,43 +48,36 @@ function generatePassword(){
     generatePassword();
   }
 
-  //checks if the user would like to use letters. and if they do weather they want uppercase and lowercase.
+  //checks if the user would like to use letters. and if they do weather they want uppercase and lowercase and adds them to the myPassword array.
   var includeLetters = window.confirm("Do you want letters in your password?\n" + "Click okay for yes and cancel for no.");
   if(includeLetters){
-    includeUpperCase = window.confirm("Would you like uppercase letters?\n" + "Click okay for yes and cancel for no.");
-    if(includeUpperCase)passwordArraySelction.push("uppercase");
-    includeLowerCase = window.confirm("Would you like lowercase letters?\n" + "Click okay for yes and cancel for no.");
-    if(includeLowerCase)passwordArraySelction.push("lowercase");
+    var includeUpperCase = window.confirm("Would you like uppercase letters?\n" + "Click okay for yes and cancel for no.");
+    if(includeUpperCase)myPassword = myPassword.concat(alphabetUpperCase);
+    var includeLowerCase = window.confirm("Would you like lowercase letters?\n" + "Click okay for yes and cancel for no.");
+    if (includeLowerCase)myPassword = myPassword.concat(alphabetLowerCase);
   }
 
-  //checks weather the user would like to user numbers in the password.
+  //asks if numbers should be included and adds them to the myPassword array if true.
   var includeNumbers = window.confirm("Do you want numbers in your password?\n" + "Click okay for yes and cancel for no.");
-  if(includeNumbers)passwordArraySelction.push("numbers");
+  if (includeNumbers)myPassword = myPassword.concat(numbersArray);
 
+  //asks if speciala characters are to be included and if true add the to the my password array.
   var includeSpecialCharacters = window.confirm("Do you want Special Characters. in your password?\n" + "Click okay for yes and cancel for no.");
-  if(includeSpecialCharacters)passwordArraySelction.push("specialcharacters");
-  
-  //if includeuppercase is true add the uppercase alphabet to the my password array.
-  if(passwordArraySelction.indexOf("uppercase") !== -1){
-    myPassword.push(alphabetUpperCase);
-    console.log(myPassword);
+  if (includeSpecialCharacters)myPassword = myPassword.concat(specialCharArray);
+
+  if(myPassword.length === 0){
+
+  window.alert("you have selected no options.\n" +
+  "please press Generate Password to start again."
+  );
+    return ;
   }
 
-  if(passwordArraySelction.indexOf("lowercase") !== -1){
-    myPassword.push(alphabetLowerCase);
-  }
+  myPassword = shuffleArray(myPassword);
+  myPassword = randomSelection(myPassword, lengthOfPassword);
+  myPassword = myPassword.join("");
 
-  if(passwordArraySelction.indexOf("numbers") !== -1){
-    myPassword.push(numbersArray);
-  }
-
-  if(passwordArraySelction.indexOf("specialcharacters") !== -1){
-    myPassword.push(specialCharArray);  
-  }
-
-  console.log(myPassword[25]);
-
-  return password;
+  return myPassword;
 }
 
 // Write password to the #password input
